47f42eeabd1915b79990e1cc9395031615acd89c
zengyg <zengyg@163.com>
2016-09-17 16:12:08 +0800

    modify the wrong test case

--- project/test/com.zte/PrimeCalculatorTest.java
+++ project/test/com.zte/PrimeCalculatorTest.java
@@ -79,7 +79,7 @@ public class PrimeCalculatorTest {
     }
 
     @Test
-    public void should_return_true_when_input_4() {
+    public void should_return_false_when_input_4() {
         assertThat(calculator.isPrime(4), is(false));
     }
 
